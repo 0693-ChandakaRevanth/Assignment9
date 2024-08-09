20c784904fb87e67d5d1002bbf38f1c1308b205d
Revanth  Chandaka <Revanth.Chandaka@godigit.com>
2024-08-09 15:05:17 +0530

    Assignment_9

--- Try_With_MultipleResources/Try_With_MultipleResources.java
+++ Try_With_MultipleResources/Try_With_MultipleResources.java
@@ -1,16 +1,15 @@
 package Try_With_MultipleResources;
-
 import java.io.*;
 public class Try_With_MultipleResources {
         public static void main(String[] args) {
-            File file1 = new File("file1.txt");
+            File file1 = new File("Try_With_MultipleResources/file1.txt");
             try {
                 if (file1.createNewFile())
                     System.out.println(file1.getName() + " created");
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            File file2 = new File("file2.txt");
+            File file2 = new File("Try_With_MultipleResources/file2.txt");
             try {
                 if (file2.createNewFile())
                     System.out.println(file2.getName() + " created");
@@ -24,9 +23,9 @@ public class Try_With_MultipleResources {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            try (BufferedReader reader1 = new BufferedReader(new FileReader("file1.txt"));
-                 BufferedReader reader2 = new BufferedReader(new FileReader("file2.txt"));
-                 BufferedWriter writer = new BufferedWriter(new FileWriter("Checked_Unchecked/destination.txt"))) {
+            try (BufferedReader reader1 = new BufferedReader(new FileReader("Try_With_MultipleResources/file1.txt"));
+                 BufferedReader reader2 = new BufferedReader(new FileReader("Try_With_MultipleResources/file2.txt"));
+                 BufferedWriter writer = new BufferedWriter(new FileWriter("Try_With_MultipleResources/destination.txt"))) {
                 String line;
                 while ((line = reader1.readLine()) != null) {
                     writer.write(line);
